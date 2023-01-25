ecea1268ffcdd7a04391781503f5b0c5f5eb6162
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-01-25 14:53:32 -0600

    Some functionality

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let mikoArr = ["Curious", "Stubborn", "Skittish"];
-let lunaArr = ["Cuddly", "Vocal", "Chill"]
+let lunaArr = ["Cuddly", "Vocal", "Chill"];
 
 let aboutMiko = document.getElementById("about-miko");
 let aboutLuna = document.getElementById("about-luna");
 
 function aboutM() {
-    for (let i = 0; i < mikoArr.length, i++) {
-        aboutMiko.innerHTML += mikoArr[i];
-    }
+  for (let i = 0; i < mikoArr.length; i++) {
+    aboutMiko.innerHTML += "<li>" + mikoArr[i] + "</li>";
+  }
+}
+
+function aboutL() {
+  for (let i = 0; i < lunaArr.length; i++) {
+    aboutLuna.innerHTML += "<li>" + lunaArr[i] + "</li>";
+  }
 }
